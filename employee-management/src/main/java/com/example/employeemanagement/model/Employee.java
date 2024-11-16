afb85e9fcbd20f1fba9647f8502a9f3a2f33601c
shaileshajabe <shaileshajabe2013@gmail.com>
2024-11-16 13:17:26 +0530

    new changes

--- employee-management/src/main/java/com/example/employeemanagement/model/Employee.java
+++ employee-management/src/main/java/com/example/employeemanagement/model/Employee.java
@@ -15,12 +15,17 @@ public class Employee {
     
     private Long id;
     public Long getId() {
+    	
 		return id;
+		
 	}
 
 	public void setId(Long id) {
 		this.id = id;
+		int num=0;
 	}
+	
+	
 
 	public String getFirstName() {
 		return firstName;
